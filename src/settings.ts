3519f487862265568c84c5882353b5fb51a6ead9
Sky Chen <icksky4@gmail.com>
2020-10-19 20:58:25 +0800

    add showSidebar & showNavbar to settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,13 @@
 interface Settings {
   title: string
+  showSidebar: boolean
+  showNavbar: boolean
 }
 
 const settings: Settings = {
   title: 'Quasar Admin',
+  showSidebar: true,
+  showNavbar: true,
 }
 
 export default settings
